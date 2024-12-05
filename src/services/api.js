d72ceac4c4dbf77135de50805553491163b58ebd
prasadpathivada <155958450+prasadpathivada@users.noreply.github.com>
2024-12-05 12:39:14 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api', // Your backend API base URL
+    baseURL: 'https://qrscan-latest.onrender.com/api', // Your backend API base URL
 });
 
 export default api;
